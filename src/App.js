e089263a0938cbdb4b6552414ab35dd2b6f21ede
eddiemuhoro <eddiemuhoro@gmail.com>
2023-10-03 15:46:43 +0300

    removed color palletes

--- src/App.js
+++ src/App.js
@@ -9,12 +9,17 @@ import Map from './components/Map';
 import Footer from './components/Footer/Jfooter';
 import Comment from './components/comments/Comment';
 import Rooms from './components/Rooms/Rooms';
-import Colors from './components/tests/Colors';
 function App() {
   return (
     <div className="App">
      <NavBar />
-    <Colors />
+     <Home  />
+     <Services/>
+     <About />
+     <Gallery />
+     <Rooms />
+      <Comment />
+     <Contact />
      <Map/>
      <Footer />
     </div>
